5e3f595d3bda0386fa1fd18a731e942b37d220fa
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2021-01-27 07:46:10 +0200

    add changes

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.db.models import Avg
+import numpy as np
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    profile_photo = models.ImageField(upload_to='profiles/',null=True)
+    bio = models.CharField(max_length=240, null=True)
+    phone = models.PositiveIntegerField(default=0)
+
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+        post_save.connect(create_user_profile, sender=User)
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def get_profile(cls):
+        profile = Profile.objects.all()
+
+        return profile
+
+
+
+class Project(models.Model):
+    posted_by = models.ForeignKey(User, null=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE,null=True)
+    title = models.CharField(max_length=100, null=True)
+    project_image = models.ImageField(upload_to='projects/',null=True)
+    description = models.TextField(null=True)
+    project_link = models.TextField(null=True)
